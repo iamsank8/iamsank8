a45bfb1fabbb01cc9c699db16eceacc9420aabb0
Sanket <sanket@gmail.com>
2025-09-27 16:52:05 +0530

    Angular upgrade: Update Angular Material to v15

--- src/app/features/contact/contact.component.ts
+++ src/app/features/contact/contact.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { UntypedFormBuilder, UntypedFormGroup, Validators } from '@angular/forms';
-import { MatSnackBar } from '@angular/material/snack-bar';
+import { MatLegacySnackBar as MatSnackBar } from '@angular/material/legacy-snack-bar';
 import { ContactService } from '../../core/services/contact.service';
 
 @Component({
